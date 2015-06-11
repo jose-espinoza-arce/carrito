3a754512b1cf33a0afb2362484d615e877568cf7
Jose <backend2.muchaweb@gmail.com>
2015-06-11 12:37:55 -0500

    fix statics

--- maravatio/settings.py
+++ maravatio/settings.py
@@ -393,9 +393,9 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 
 #from oscar.defaults import *
--- static2/app/service/service.module.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	'use strict';
-
-	angular
-		.module('app.service', [])
-		.constant('baseUrl', 'http://localhost:8000/dashboard/api/');
-    //token:c4e913f4877e6762b8458b4d349ed402a5c3a842
-
-})();
