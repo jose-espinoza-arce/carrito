b80198e28a1fded8828445730abe9c7250627e71
Jose <backend2.muchaweb@gmail.com>
2015-06-18 16:00:55 -0500

    change home to index

--- promotions/views.py
+++ promotions/views.py
@@ -3,6 +3,8 @@ from oscar.apps.promotions.views import HomeView as CoreHomeView
 
 class HomeView(CoreHomeView):
 
+    template_name = 'promotions/index.html'
+
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         request.META["CSRF_COOKIE_USED"] = True
